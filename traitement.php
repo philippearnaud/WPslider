2ed19217a4f27ad3cc244e81abbd11d77ea1af00
philippearnaud <philippe.demangou@gmail;com>
2014-09-01 08:31:09 +0200

    add le fichier traitement php

--- traitement.php
+++ traitement.php
@@ -1,11 +1,10 @@
 <?php 
 
-//TODO: Ne pas oublier de mettre ce fichier dans /wp-admin.
-//TODO: Faire un script pour qu'il soit dès l'activation et retiré à la désactivation.
 require_once('../wp-load.php');
 $titre = htmlspecialchars($_POST['titre']);
 $description = htmlspecialchars($_POST['description']);
 $image = htmlspecialchars($_POST['upload_image']);
+var_dump($titre, $description, $image);
 var_dump($titre, $description, $image);
 
 function add_entries($titre, $description, $image) {
