df97aa300911c3f7e895f95aafe6201665c994a2
philippearnaud <philippe.demangou@gmail.com>
2014-09-01 16:00:11 +0200

    Evènement permettant de modifier le contenu d'une description à la volée

--- update_script.js
+++ update_script.js
@@ -1,10 +1,17 @@
 jQuery(function() {
-    var description = jQuery('.description_slide', this).text();
     var image = jQuery('.image_slide', this).text();
 
     jQuery('h4.titre_slide').dblclick(function(ev) {
         var titre = jQuery(this).text();
         jQuery(this).after("<input type='text' name='titre' value='" + titre + "'>").remove();
     });
-    
+
+    jQuery('.description_slide').dblclick(function(ev) {
+        var description = jQuery('.description_slide', this).text();
+        jQuery(this).after("<textarea name='description' value='" + description + "' row='50' cols='50'>").remove();
+    });
+
+        
+
+    });
 });
